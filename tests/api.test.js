a47f96035614b5a85b1af3fe632881da1e18f3d0
rori-bdg <rori.bdg@gmail.com>
2020-06-30 03:10:12 +0700

    Fixed nyc code coverage (#9)
    * Fixed nyc code coverage

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,12 +1,24 @@
 'use strict'
 
 const request = require('supertest')
+const assert = require('assert')
+const sinon = require('sinon')
 
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database(':memory:')
 
 const app = require('../src/app')(db)
 const buildSchemas = require('../src/schemas')
+
+const rideRequest = {
+  start_lat: 10,
+  start_long: 50,
+  end_lat: 20,
+  end_long: 70,
+  rider_name: 'Test Rider',
+  driver_name: 'Test Driver',
+  driver_vehicle: 'Test Vehicle'
+}
 
 describe('API tests', () => {
   before((done) => {
@@ -21,12 +33,256 @@ describe('API tests', () => {
     })
   })
 
+  beforeEach((done) => {
+    db.run('DELETE FROM Rides', () => {
+      done()
+    })
+  })
+
   describe('GET /health', () => {
     it('should return health', (done) => {
       request(app)
         .get('/health')
         .expect('Content-Type', /text/)
         .expect(200, done)
+    })
+
+    it('should return not found status when getting invalid URL', (done) => {
+      request(app)
+        .get('/healthy')
+        .expect(404, done)
+    })
+  })
+
+  describe('POST /rides', () => {
+    it('should return added ride', (done) => {
+      request(app)
+        .post('/rides')
+        .send(rideRequest)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect((response) => {
+          assert.equal(response.body.length, 1, 'Expected 1 ride.')
+          assert.equal(response.body[0].startLat, rideRequest.start_lat, 'Expected startLat ' + rideRequest.start_lat + '.')
+        })
+        .then(done())
+    })
+
+    it('should return UNPROCESSABLE_ENTITY for invalid start_lat', (done) => {
+      var invalidRideRequest = JSON.parse(JSON.stringify(rideRequest))
+      invalidRideRequest.start_lat = -500
+
+      request(app)
+        .post('/rides')
+        .send(invalidRideRequest)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(422, done)
+    })
+
+    it('should return UNPROCESSABLE_ENTITY for invalid start_long', (done) => {
+      var invalidRideRequest = JSON.parse(JSON.stringify(rideRequest))
+      invalidRideRequest.start_long = -500
+
+      request(app)
+        .post('/rides')
+        .send(invalidRideRequest)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(422, done)
+    })
+
+    it('should return UNPROCESSABLE_ENTITY for invalid end_lat', (done) => {
+      var invalidRideRequest = JSON.parse(JSON.stringify(rideRequest))
+      invalidRideRequest.end_lat = -500
+
+      request(app)
+        .post('/rides')
+        .send(invalidRideRequest)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(422, done)
+    })
+
+    it('should return UNPROCESSABLE_ENTITY for invalid end_long', (done) => {
+      var invalidRideRequest = JSON.parse(JSON.stringify(rideRequest))
+      invalidRideRequest.end_long = -500
+
+      request(app)
+        .post('/rides')
+        .send(invalidRideRequest)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(422, done)
+    })
+
+    it('should return UNPROCESSABLE_ENTITY for empty rider_name', (done) => {
+      var invalidRideRequest = JSON.parse(JSON.stringify(rideRequest))
+      invalidRideRequest.rider_name = ''
+
+      request(app)
+        .post('/rides')
+        .send(invalidRideRequest)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(422, done)
+    })
+
+    it('should return UNPROCESSABLE_ENTITY for empty driver_name', (done) => {
+      var invalidRideRequest = JSON.parse(JSON.stringify(rideRequest))
+      invalidRideRequest.driver_name = ''
+
+      request(app)
+        .post('/rides')
+        .send(invalidRideRequest)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(422, done)
+    })
+
+    it('should return UNPROCESSABLE_ENTITY for empty driver_vehicle', (done) => {
+      var invalidRideRequest = JSON.parse(JSON.stringify(rideRequest))
+      invalidRideRequest.driver_vehicle = ''
+
+      request(app)
+        .post('/rides')
+        .send(invalidRideRequest)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(422, done)
+    })
+
+    it('should return INTERNAL_SERVER_ERROR when server error upon INSERT', (done) => {
+      var runStub = sinon.stub(db, 'run').yields(Error('Database INSERT failed'), null)
+
+      request(app)
+        .post('/rides')
+        .send(rideRequest)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(500)
+        .expect((response) => {
+          assert.equal(response.body.error_code, 'SERVER_ERROR', 'Expected SERVER_ERROR.')
+        })
+        .then(() => {
+          done()
+          runStub.restore()
+        })
+    })
+
+    it('should return INTERNAL_SERVER_ERROR when server error upon SELECT', (done) => {
+      var allStub = sinon.stub(db, 'all').yields(Error('Database SELECT failed'), null)
+
+      request(app)
+        .post('/rides')
+        .send(rideRequest)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(500)
+        .expect((response) => {
+          assert.equal(response.body.error_code, 'SERVER_ERROR', 'Expected SERVER_ERROR.')
+        })
+        .then(() => {
+          done()
+          allStub.restore()
+        })
+    })
+  })
+
+  describe('GET /rides', () => {
+    it('should return list of rides', (done) => {
+      request(app)
+        .post('/rides')
+        .send(rideRequest)
+        .set('Accept', 'application/json')
+        .then(() => {
+          request(app)
+            .post('/rides')
+            .send(rideRequest)
+            .set('Accept', 'application/json')
+            .then(() => {
+              request(app)
+                .get('/rides')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .expect((response) => {
+                  assert.equal(response.body.length, 2, 'Expected 2 rides.')
+                })
+                .then(done())
+            })
+        })
+    })
+
+    it('should return BAD_REQUEST for no rides found', (done) => {
+      request(app)
+        .get('/rides')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(400, done)
+    })
+
+    it('should return INTERNAL_SERVER_ERROR when server error upon SELECT', (done) => {
+      var allStub = sinon.stub(db, 'all').yields(Error('Database SELECT failed'), null)
+
+      request(app)
+        .get('/rides')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(500)
+        .expect((response) => {
+          assert.equal(response.body.error_code, 'SERVER_ERROR', 'Expected SERVER_ERROR.')
+        })
+        .then(() => {
+          done()
+          allStub.restore()
+        })
+    })
+  })
+
+  describe('GET /rides/:id', () => {
+    it('should return a ride by ID', (done) => {
+      request(app)
+        .post('/rides')
+        .send(rideRequest)
+        .set('Accept', 'application/json')
+        .then((response) => {
+          request(app)
+            .get('/rides/' + response.body[0].rideID)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect((response) => {
+              assert.equal(response.body.length, 1, 'Expected 1 ride.')
+            })
+            .then(done())
+        })
+    })
+
+    it('should return BAD_REQUEST for no rides found', (done) => {
+      request(app)
+        .get('/rides/1')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(400, done)
+    })
+
+    it('should return INTERNAL_SERVER_ERROR when server error upon SELECT', (done) => {
+      var allStub = sinon.stub(db, 'all').yields(Error('Database SELECT failed'), null)
+
+      request(app)
+        .get('/rides/1')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(500)
+        .expect((response) => {
+          assert.equal(response.body.error_code, 'SERVER_ERROR', 'Expected SERVER_ERROR.')
+        })
+        .then(() => {
+          done()
+          allStub.restore()
+        })
     })
   })
 })
